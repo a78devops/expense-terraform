904318a923e5328a7f408310794b8d96235f8dd2
pajjuriabhishek <pajjuriabhishek55@gmail.com>
2024-07-25 17:30:50 +0530

    adding natgw and routes

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -25,6 +25,58 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "${var.env}-public-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table" "public" {
+  count  = length(var.public_subnets)
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block                = var.default_vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    name = "${var.env}-public-rt-${count.index + 1}"
+  }
+
+}
+
+resource "aws_eip" "ngw" {
+  count = length(var.public_subnets)
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "ngw" {
+  count = length(var.public_subnets)
+  allocation_id = aws_eip.ngw[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+
+  tags = {
+    Name = "${var.env}-ngw-${count.inex + 1}"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public[count.index].id
+}
+
 resource "aws_subnet" "frontend" {
   count             = length(var.frontend_subnets)
   vpc_id            = aws_vpc.main.id
@@ -43,6 +95,11 @@ resource "aws_route_table" "frontend" {
   route {
     cidr_block                = var.default_vpc_cidr
     vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  }
+
+  route {
+    cidr_block                = "0.0.0.0/0"
+    vpc_peering_connection_id = aws_nat_gateway.ngw[count.index].id
   }
 
   tags = {
@@ -77,6 +134,11 @@ resource "aws_route_table" "backend" {
     vpc_peering_connection_id = aws_vpc_peering_connection.main.id
   }
 
+  route {
+    cidr_block                = "0.0.0.0/0"
+    vpc_peering_connection_id = aws_nat_gateway.ngw[count.index].id
+  }
+
   tags = {
     name = "${var.env}-backend-rt-${count.index + 1}"
   }
@@ -88,6 +150,7 @@ resource "aws_route_table_association" "backend" {
   subnet_id      = aws_subnet.backend[count.index].id
   route_table_id = aws_route_table.backend[count.index].id
 }
+
 
 resource "aws_subnet" "db" {
   count             = length(var.db_subnets)
@@ -109,6 +172,11 @@ resource "aws_route_table" "db" {
     vpc_peering_connection_id = aws_vpc_peering_connection.main.id
   }
 
+  route {
+    cidr_block                = "0.0.0.0/0"
+    vpc_peering_connection_id = aws_nat_gateway.ngw[count.index].id
+  }
+
   tags = {
     name = "${var.env}-db-rt-${count.index + 1}"
   }
@@ -121,43 +189,9 @@ resource "aws_route_table_association" "db" {
   route_table_id = aws_route_table.db[count.index].id
 }
 
-resource "aws_subnet" "public" {
-  count             = length(var.public_subnets)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnets[count.index]
-  availability_zone = var.availability_zones[count.index]
-
-  tags = {
-    Name = "${var.env}-public-subnet-${count.index + 1}"
-  }
-}
-
-resource "aws_route_table" "public" {
-  count             = length(var.public_subnets)
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = var.default_vpc_cidr
-    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
-  }
-
-  tags = {
-    name = "${var.env}-public-rt-${count.index + 1}"
-  }
-
-}
-
-resource "aws_route_table_association" "public" {
-  count          = length(var.public_subnets)
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public[count.index].id
-}
-
 
 resource "aws_route" "default-vpc" {
   route_table_id            = var.default_route_table_id
   vpc_peering_connection_id = aws_vpc_peering_connection.main.id
   destination_cidr_block    = var.vpc_cidr_block
 }
-
-
