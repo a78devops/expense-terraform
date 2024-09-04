b0ffa16099a6e445bfb403aa162388e42df53a73
pajjuriabhishek <pajjuriabhishek55@gmail.com>
2024-09-04 11:53:05 +0530

    update rds details in backend and terraform rds

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -69,7 +69,7 @@ resource "null_resource" "ansible" {
     inline = [
       "sudo pip3.11 install ansible hvac",
       "ansible-pull -i localhost, -U https://github.com/a78devops/expense-ansible get-secrets.yml -e env=${var.env} -e role_name=${var.component} -e vault_token=${var.vault_token}",
-      "ansible-pull -i localhost, -U https://github.com/a78devops/expense-ansible expense.yml -e env=${var.env} -e role_name=${var.component} -e @secrets.json",
+      "ansible-pull -i localhost, -U https://github.com/a78devops/expense-ansible expense.yml -e env=${var.env} -e role_name=${var.component} -e @~/secrets.json",
       "rm -f ~/secrets.json ~/app.json"
     ]
   }
