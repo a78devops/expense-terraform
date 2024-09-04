8888153154e22a9ee6c244106582ad8a7f2e40f1
pajjuriabhishek <pajjuriabhishek55@gmail.com>
2024-09-04 17:36:00 +0530

    adding trigger to null resource

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -62,6 +62,9 @@ resource "aws_instance" "instance" {
 }
 
 resource "null_resource" "ansible" {
+  triggers = {
+    instance = aws_instance.instance.id
+  }
   provisioner "remote-exec" {
 
     connection {
